a4f4c996f8b23aaab20e8d6ff48436443fd8cb88
Tristan Slater <hello@trslater.ca>
2022-03-04 15:25:03 -0800

    Create basic piece data model

---
+++ chess/pieces.py
@@ -0,0 +1,81 @@
+from functools import total_ordering
+from math import inf
+
+
+@total_ordering
+class Piece:
+    __slots__ = ("VALUE",)
+    
+    def __lt__(self, other) -> bool:
+        """Needed for `total_ordering` and more generally for comparing
+        value of pieces"""
+
+        return self.VALUE < other.VALUE
+    
+    def __eq__(self, other) -> bool:
+        """Needed for `total_ordering` and more generally for comparing
+        value of pieces"""
+
+        return self.VALUE == other.VALUE
+
+    def __radd__(self, other):
+        """Facilitates summing, used for totalling points"""
+
+        if isinstance(other, int):
+            return self.VALUE + other
+
+        return self.VALUE + other.VALUE
+
+
+class Pawn(Piece):
+    VALUE = 1
+
+    def __str__(self) -> str:
+        return (" (_)\n"
+                " / \\\n"
+                "-----")
+
+
+class Knight(Piece):
+    VALUE = 3
+
+    def __str__(self) -> str:
+        return ("/_o^\\\n"
+                " /  /\n"
+                "-----")
+
+
+class Bishop(Piece):
+    VALUE = 3
+
+    def __str__(self) -> str:
+        return (" //\\\n"
+                "(/  )\n"
+                "-----")
+
+
+class Rook(Piece):
+    VALUE = 5
+
+    def __str__(self) -> str:
+        return ("|_|_|\n"
+                "/   \\\n"
+                "-----")
+
+
+class Queen(Piece):
+    VALUE = 9
+
+    def __str__(self) -> str:
+        return ("\\\\|//\n"
+                " \\ /\n"
+                "-----")
+
+
+class King(Piece):
+    VALUE = inf
+
+    def __str__(self) -> str:
+        return ("  +\n"
+                "( | )\n"
+                "-----")
