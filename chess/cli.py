27ef4946b17d978b9d4c87fc59ecaf34a7ee93cc
Tristan Slater <hello@trslater.ca>
2022-03-06 03:03:58 -0800

    Cycle players and accept input

--- chess/cli.py
+++ chess/cli.py
@@ -1,2 +1,15 @@
+import itertools
+import random
+
+from chess.player import Player
+
+
 def run():
-    pass
+    players = random.sample((Player("One"), Player("Two")), k=2)
+
+    try:
+        for player in itertools.cycle(players):
+            input(f"{player.name}'s turn: ")
+    
+    except KeyboardInterrupt:
+        pass
